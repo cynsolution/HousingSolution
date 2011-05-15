b935d90975c6e955af59152b4de5dda8a75e6668
Pankaj <pankaj@cynsolution.com>
2011-05-16 00:46:11 +0530

    connection string is added in web.config

--- HousingSolution/HousingSolution/Application.aspx.cs
+++ HousingSolution/HousingSolution/Application.aspx.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration
 
 namespace HousingSolution
 {
     public partial class Application : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadButton1_Click(object sender, EventArgs e)
         {
 
         }
